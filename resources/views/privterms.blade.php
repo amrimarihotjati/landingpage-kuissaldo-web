5f6df1a0e965c6cb63fa80047cfac38175b4d307
SaniAsJounin <jounintechnology.developer@gmail.com>
2024-08-30 00:09:17 +0700

    checkpoint finish static

--- resources/views/privterms.blade.php
+++ resources/views/privterms.blade.php
@@ -29,7 +29,8 @@
         <div class="container px-5">
             <a class="navbar-brand fw-bold" href="#page-top">{{ env('app_name', 'Kuis Saldo') }}</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa-solid fa-bars"></i>
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa-solid fa-bars"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
